54f202d00d20148457c0946c390fed640bf62fee
Fedeeico Albano <federico.albano2@studio.unibo.it>
2025-08-13 11:40:08 +0200

    test

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <fstream>
 #include <iostream>
 
-#include "simulation.hpp" // !!!ho cambiato in cpp perchè altrimenti non mi girava, poi ne parliamo, e mi dici come lo mandavi te
+#include "simulation.cpp"
 
 int main()
 {
@@ -16,7 +16,7 @@ int main()
     std::cout << "=== Volterra simulation ===\n";
 
     std::cout << "Enter A (prey reproduction rate):\n";
-    std::cin >> A;
+    // std::cin >> A;
 
     if (!(std::cin >> A)) {
       std::cerr << "Error: parameter must be a rational number" << std::endl;
@@ -24,7 +24,7 @@ int main()
     }
 
     std::cout << "Enter B (prey mortality rate):\n";
-    std::cin >> B;
+    // std::cin >> B;
 
     if (!(std::cin >> B)) {
       std::cerr << "Error: parameter must be a rational number" << std::endl;
@@ -32,7 +32,7 @@ int main()
     }
 
     std::cout << "Enter C (predator reproduction rate):\n";
-    std::cin >> C;
+    // std::cin >> C;
 
     if (!(std::cin >> C)) {
       std::cerr << "Error: parameter must be a rational number" << std::endl;
@@ -40,7 +40,7 @@ int main()
     }
 
     std::cout << "Enter D (predator mortality rate):\n";
-    std::cin >> D;
+    // std::cin >> D;
 
     if (!(std::cin >> D)) {
       std::cerr << "Error: parameter must be a rational number" << std::endl;
@@ -48,7 +48,7 @@ int main()
     }
 
     std::cout << "Enter x0 (initial prey population density):\n";
-    std::cin >> x0;
+    // std::cin >> x0;
 
     if (!(std::cin >> x0)) {
       std::cerr << "Error: parameter must be a rational number" << std::endl;
@@ -56,30 +56,30 @@ int main()
     }
 
     std::cout << "Enter y0 (initial predator population density):\n";
-    std::cin >> y0;
+    // std::cin >> y0;
 
     if (!(std::cin >> y0)) {
       std::cerr << "Error: parameter must be a rational number" << std::endl;
       exit(EXIT_FAILURE);
-    } else {
-
-      std::cout << "Enter time step dt:\n";
-      std::cin >> dt;
-
-      if (!(std::cin >> dt)) {
-        std::cerr << "Error: parameter must be a rational number" << std::endl;
-        exit(EXIT_FAILURE);
-      }
-
-      std::cout << "Enter number of steps:\n";
-      std::cin >> steps;
-
-      if (!(std::cin >> steps)) {
-        std::cerr << "Error: must enter a integer number of steps" << std::endl;
-        exit(EXIT_FAILURE);
-      } // ! nega il valore booleano, il blocco if viene letto solo se la
-        // lettura fallisce
     }
+
+    std::cout << "Enter time step dt:\n";
+    // std::cin >> dt;
+
+    if (!(std::cin >> dt)) {
+      std::cerr << "Error: parameter must be a rational number" << std::endl;
+      exit(EXIT_FAILURE);
+    }
+
+    std::cout << "Enter number of steps:\n";
+    // std::cin >> steps;
+
+    if (!(std::cin >> steps)) {
+      std::cerr << "Error: must enter a integer number of steps" << std::endl;
+      exit(EXIT_FAILURE);
+    } // ! nega il valore booleano, il blocco if viene letto solo se la
+      // lettura fallisce
+
     Simulation sim(A, B, C, D, x0, y0, dt);
 
     sim.run(steps);
