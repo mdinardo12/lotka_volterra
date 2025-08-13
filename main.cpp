7518323a409acb760f35d32c8191b423639ff907
Fedeeico Albano <federico.albano2@studio.unibo.it>
2025-08-13 11:14:47 +0200

    test

--- main.cpp
+++ main.cpp
@@ -15,15 +15,15 @@ int main()
 
     std::cout << "=== Volterra simulation ===\n";
 
-    std::cout << "Enter A (prey reproduction rate): ";
+    std::cout << "Enter A (prey reproduction rate):\n";
     std::cin >> A;
 
     if (!(std::cin >> A)) {
-      std::cerr << "Error: parameter must be a rational number";
+      std::cerr << "Error: parameter must be a rational number" << std::endl;
       exit(EXIT_FAILURE);
     }
 
-    std::cout << "Enter B (prey mortality rate): ";
+    std::cout << "Enter B (prey mortality rate):\n";
     std::cin >> B;
 
     if (!(std::cin >> B)) {
@@ -31,7 +31,7 @@ int main()
       exit(EXIT_FAILURE);
     }
 
-    std::cout << "Enter C (predator reproduction rate): ";
+    std::cout << "Enter C (predator reproduction rate):\n";
     std::cin >> C;
 
     if (!(std::cin >> C)) {
@@ -39,7 +39,7 @@ int main()
       exit(EXIT_FAILURE);
     }
 
-    std::cout << "Enter D (predator mortality rate): ";
+    std::cout << "Enter D (predator mortality rate):\n";
     std::cin >> D;
 
     if (!(std::cin >> D)) {
@@ -47,7 +47,7 @@ int main()
       exit(EXIT_FAILURE);
     }
 
-    std::cout << "Enter x0 (initial prey population density): ";
+    std::cout << "Enter x0 (initial prey population density):\n";
     std::cin >> x0;
 
     if (!(std::cin >> x0)) {
@@ -55,7 +55,7 @@ int main()
       exit(EXIT_FAILURE);
     }
 
-    std::cout << "Enter y0 (initial predator population density): ";
+    std::cout << "Enter y0 (initial predator population density):\n";
     std::cin >> y0;
 
     if (!(std::cin >> y0)) {
@@ -63,7 +63,7 @@ int main()
       exit(EXIT_FAILURE);
     } else {
 
-      std::cout << "Enter time step dt: ";
+      std::cout << "Enter time step dt:\n";
       std::cin >> dt;
 
       if (!(std::cin >> dt)) {
@@ -71,7 +71,7 @@ int main()
         exit(EXIT_FAILURE);
       }
 
-      std::cout << "Enter number of steps: ";
+      std::cout << "Enter number of steps:\n";
       std::cin >> steps;
 
       if (!(std::cin >> steps)) {
